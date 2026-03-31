ce661f85113e5b948cf25029d7c47a556a0fddc3
hgnodes <hgnodes02@gmail.com>
2026-03-31 23:04:34 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,20 +1,7 @@
 #!/bin/bash
 
-export DEBIAN_FRONTEND=noninteractive
+echo "[INFO] Starting VPS..."
 
-echo "[INFO] Starting VPS Setup..."
-
-echo "[INFO] Updating system..."
-apt update 
-
-echo "[INFO] Fixing timezone (no prompt)..."
-ln -fs /usr/share/zoneinfo/Asia/Kolkata /etc/localtime
-dpkg-reconfigure --frontend noninteractive tzdata
-
-echo "[INFO] Installing packages..."
-apt install -y curl wget sudo git unzip tar tmate
-
-echo "[INFO] Starting tmate..."
 tmate -F > /dev/null 2>&1 &
 
 sleep 8
@@ -24,10 +11,7 @@ tmate display -p '#{tmate_ssh}'
 tmate display -p '#{tmate_web}'
 echo "================================="
 
-echo "[SUCCESS] VPS READY!"
-
-# fake web server for Render (IMPORTANT)
-echo "[INFO] Starting dummy server on port 10000..."
+# keep alive + port for render
 while true; do
   echo -e "HTTP/1.1 200 OK\n\nVPS Running" | nc -l -p 10000 -q 1
 done
